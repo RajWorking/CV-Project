94355000296bb81e0d45f36656a74c6f2ebb98f6
Cheif-Blackhood <ppratap2001@gmail.com>
2022-04-18 15:08:35 +0530

    Data Generator done

--- model/class_rebalancing.py
+++ model/class_rebalancing.py
@@ -1,5 +1,4 @@
 import os
-from turtle import color
 
 import numpy as np
 import random
@@ -8,20 +7,20 @@ import sklearn.neighbors as nn
 from scipy.interpolate import interp1d
 from scipy.signal import gaussian, convolve
 
-from config import IMAGENET_IMAGES_PATH, IMAGE_DIMENSIONS, INTERPOLATION_METHOD, NUM_OF_NEIGHBOURS, SMOOTHING_SIGMA, REBALANCING_GAMMA
+from config import IMAGENET_IMAGES_PATH, IMG_ROWS, IMG_COLS, INTERPOLATION_METHOD, NUM_OF_NEIGHBOURS_REBALANCING, SMOOTHING_SIGMA, REBALANCING_GAMMA
 
-def load_data(sample_size=10000, image_dimension=IMAGE_DIMENSIONS):
+def load_data(sample_size=10000, image_rows=IMG_ROWS, image_cols=IMG_COLS):
     images_path = IMAGENET_IMAGES_PATH
     names = [name for name in os.listdir(images_path) if name.contains('.jpg')]
     subsampled_names = random.sample(names, sample_size)
-    color_channels = np.empty((sample_size, image_dimension, image_dimension, 2))
+    color_channels = np.empty((sample_size, image_rows, image_cols, 2))
     for i, name in enumerate(subsampled_names):
         bgr = cv2.imread(f'{images_path}/{name}')
         ## If image is being downsampled
         if INTERPOLATION_METHOD == 'INTER_AREA':
-            bgr = cv2.resize(bgr, (image_dimension, image_dimension), interpolation = cv2.INTER_AREA)
+            bgr = cv2.resize(bgr, (image_rows, image_cols), interpolation = cv2.INTER_AREA)
         else:
-            bgr = cv2.resize(bgr, (image_dimension, image_dimension), interpolation = cv2.INTER_CUBIC)
+            bgr = cv2.resize(bgr, (image_rows, image_cols), interpolation = cv2.INTER_CUBIC)
         lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
         lab = lab.astype(np.int32) ## Do we need this
         color_channels[i] = lab[:, :, 1:] - 128
@@ -31,7 +30,7 @@ def compute_color_prior_factor(color_channels, sigma=SMOOTHING_SIGMA, gamma=REBA
     ab_bins = np.load('data/pts_in_lab_space.npy')
     color_channels = color_channels.reshape(-1, 2)
     ## Find bins for each color pixels
-    nearest = nn.NearestNeighbors(n_neighbors=NUM_OF_NEIGHBOURS, algorithm='ball_tree').fit(ab_bins) # Can use algorithm='auto'
+    nearest = nn.NearestNeighbors(n_neighbors=NUM_OF_NEIGHBOURS_REBALANCING, algorithm='ball_tree').fit(ab_bins) # Can use algorithm='auto'
     _, indexs = nearest.kneighbours(color_channels)
     pos_prob = np.bincount(indexs.flatten())
     pos_indxes = np.unique(indexs)
