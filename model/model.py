3f1722997917af705a341f2984726e6611c97824
Cheif-Blackhood <ppratap2001@gmail.com>
2022-04-20 12:00:06 +0530

    resolving  merge conflict

--- model/model.py
+++ model/model.py
@@ -1,13 +1,14 @@
 import keras.backend as K
 import tensorflow as tf
-from tensorflow import keras
-from keras.layers import Conv2D, BatchNormalization, UpSampling2D
+# from tensorflow import keras
+from keras.layers import Conv2D, BatchNormalization, UpSampling2D, Input, Conv2DTranspose
 # from keras.models import Model
 # from keras.regularizers import l2
-from keras.utils import multi_gpu_model
-from keras.utils import plot_model
+from tensorflow.keras import Sequential
+from tensorflow.python.keras.utils.multi_gpu_utils import multi_gpu_model
+# from tensorflow.keras.utils import plot_model
 
-# from config import img_rows, img_cols, num_classes, kernel
+from config import IMG_ROWS, IMG_COLS
 
 # l2_reg = l2(1e-3)
 
@@ -22,61 +23,63 @@ def build_model(kernel=3):
     const_params = {
         'activation': 'relu',
         'use_bias': True,
-        'padding': 'same'
+        'padding': 'same',
+        'kernel_size': 3
     }
 
-    model = keras.Sequential([
+    model = Sequential([
+        Input(shape=(IMG_ROWS, IMG_COLS, 1)),
 
-        Conv2D(64, kernel, strides=1, dilation_rate=1, name='conv1_1', **const_params),
-        Conv2D(64, kernel, strides=2, dilation_rate=1, name='conv1_2', **const_params),
+        Conv2D(64, strides=1, dilation_rate=1, name='conv1_1', **const_params),
+        Conv2D(64, strides=2, dilation_rate=1, name='conv1_2', **const_params),
         BatchNormalization(),
 
 
-        Conv2D(128, kernel, strides=1, dilation_rate=1, name='conv2_1', **const_params),
-        Conv2D(128, kernel, strides=2, dilation_rate=1, name='conv2_2', **const_params),
+        Conv2D(128, strides=1, dilation_rate=1, name='conv2_1', **const_params),
+        Conv2D(128, strides=2, dilation_rate=1, name='conv2_2', **const_params),
         BatchNormalization(),
 
 
-        Conv2D(256, kernel, strides=1, dilation_rate=1, name='conv3_1', **const_params),
-        Conv2D(256, kernel, strides=1, dilation_rate=1, name='conv3_2', **const_params),
-        Conv2D(256, kernel, strides=2, dilation_rate=1, name='conv3_3', **const_params),
+        Conv2D(256, strides=1, dilation_rate=1, name='conv3_1', **const_params),
+        Conv2D(256, strides=1, dilation_rate=1, name='conv3_2', **const_params),
+        Conv2D(256, strides=2, dilation_rate=1, name='conv3_3', **const_params),
         BatchNormalization(),
 
 
-        Conv2D(512, kernel, strides=1, dilation_rate=1, name='conv4_1', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=1, name='conv4_2', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=1, name='conv4_3', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv4_1', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv4_2', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv4_3', **const_params),
         BatchNormalization(),
 
 
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv5_1', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv5_2', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv5_3', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv5_1', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv5_2', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv5_3', **const_params),
         BatchNormalization(),
 
         
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv6_1', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv6_2', **const_params),
-        Conv2D(512, kernel, strides=1, dilation_rate=2, name='conv6_3', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv6_1', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv6_2', **const_params),
+        Conv2D(512, strides=1, dilation_rate=2, name='conv6_3', **const_params),
         BatchNormalization(),
 
-        Conv2D(256, kernel, strides=1, dilation_rate=1, name='conv7_1', **const_params),
-        Conv2D(256, kernel, strides=1, dilation_rate=1, name='conv7_2', **const_params),
-        Conv2D(256, kernel, strides=1, dilation_rate=1, name='conv7_3', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv7_1', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv7_2', **const_params),
+        Conv2D(512, strides=1, dilation_rate=1, name='conv7_3', **const_params),
         BatchNormalization(),
 
+        Conv2DTranspose(256, kernel_size=4, strides=2, padding='same', name='conv8_1', activation='relu', use_bias=True),
+        # Conv2D(128, strides=1, dilation_rate=1, name='conv8_1', **const_params),
+        Conv2D(256, strides=1, dilation_rate=1, name='conv8_2', **const_params),
+        Conv2D(256, strides=1, dilation_rate=1, name='conv8_3', **const_params),
 
-        UpSampling2D(size=2),
-        Conv2D(128, kernel, strides=1, dilation_rate=1, name='conv8_1', **const_params),
-        Conv2D(128, kernel, strides=1, dilation_rate=1, name='conv8_2', **const_params),
-        Conv2D(128, kernel, strides=1, dilation_rate=1, name='conv8_3', **const_params),
 
+        Conv2D(313, kernel_size=1, activation='softmax', strides=1, padding='valid', name='loss_layer', use_bias=True),
 
-        Conv2D(313, 1, activation='softmax', strides=1, padding=0, name='loss_layer')
+        Conv2D(2, kernel_size=1, strides=1, dilation_rate=1, padding='valid', name='output', use_bias=False),
+        
 
     ])
-
-    print(model.summary())
 
     return model
 
@@ -85,10 +88,6 @@ if __name__ == '__main__':
     with tf.device("/cpu:0"):
         encoder_decoder = build_model()
     print(encoder_decoder.summary())
-    plot_model(encoder_decoder, to_file='encoder_decoder.svg', show_layer_names=True, show_shapes=True)
-
-    parallel_model = multi_gpu_model(encoder_decoder, gpus=None)
-    print(parallel_model.summary())
-    plot_model(parallel_model, to_file='parallel_model.svg', show_layer_names=True, show_shapes=True)
+    # plot_model(encoder_decoder, to_file='encoder_decoder.svg', show_layer_names=True, show_shapes=True)
 
     K.clear_session()
--- model/tensorfiow_imagenet_split.py
+++ model/tensorfiow_imagenet_split.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
-ds = tfds.load('imagenet2012_subset', split=['train', 'valid'], data_dir='./images', shuffle_files=True, download=True)
-assert isinstance(ds, tf.data.Dataset)
-print(ds)
+ds, ds_info = tfds.load('imagenet2012_subset/10pct', split=['train', 'validation'], data_dir='./images', shuffle_files=True, download=True, with_info=True)
+# assert isinstance(ds, tf.data.Dataset)
+# fig = tfds.show_examples(ds, ds_info)
+# print(ds)
