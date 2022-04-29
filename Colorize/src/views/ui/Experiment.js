1ca06d39e8f71b042ff1659cad848f0e0e552bcd
RajWorking <rajmaheshwari08@yahoo.com>
2022-04-29 16:01:46 +0530

    upload image

--- Colorize/src/views/ui/Experiment.js
+++ Colorize/src/views/ui/Experiment.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import bg1 from "../../assets/images/bg/bg1.jpg";
 import bg2 from "../../assets/images/bg/bg2.jpg";
 import bg3 from "../../assets/images/bg/bg3.jpg";
@@ -10,6 +11,7 @@ import {
     CardTitle,
     CardSubtitle,
     CardGroup,
+    CardDeck,
     Button,
     Row,
     Col,
@@ -17,33 +19,43 @@ import {
 
 
 const Starter = () => {
+    const [file, setFile] = useState();
+    let handleChange = (e) => {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
     return (
         <Row>
             <h3 className="mb-3 mt-3">Lets test it!</h3>
             <Col>
                 <CardGroup>
                     <Card>
-                        <CardImg alt="Card image cap" src={bg1} top width="100%" />
+                        <CardImg alt="Card image cap" src={file} top width="256px" height="256px" />
                         <CardBody>
                             <CardTitle tag="h5">Original Image</CardTitle>
                         </CardBody>
                     </Card>
                     <Card>
-                        <CardImg alt="Card image cap" src={bg2} top width="100%" />
+                        <CardImg alt="Card image cap" src={file} top width="256px" height="256px" />
                         <CardBody>
                             <CardTitle tag="h5">Grayscale image</CardTitle>
                         </CardBody>
                     </Card>
                     <Card>
-                        <CardImg alt="Card image cap" src={bg3} top width="100%" />
+                        <CardImg alt="Card image cap" src={file} top width="256px" height="256px" />
                         <CardBody>
                             <CardTitle tag="h5">Colored image</CardTitle>
                         </CardBody>
                     </Card>
                 </CardGroup>
                 <Row className="mt-3" xs={6}>
-                    <Button>Upload</Button>
-                    <Button>Convert</Button>
+                    <Button variant="contained" component="label">
+                        <input type="file" onChange={handleChange}/>
+                    </Button>
+                    <Button variant="contained">
+                        Convert
+                    </Button>
                 </Row>
             </Col>
         </Row>
